53fc20d2f2864610c37f2bc4c1e3ee783fa00e52
dword <114817148+dword32bit@users.noreply.github.com>
2024-10-08 12:20:41 +0700

    Update cloudflared.sh

--- cloudflared.sh
+++ cloudflared.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
-sudo dpkg -i cloudflared.deb
-sudo cloudflared service install eyJhIjoiNzMwNmE5OGE0NzhlNDY2MjE1NTZjMzE4YWNhMzBmNjEiLCJ0IjoiYTZiMTVkZTMtMmE1Mi00ZThhLWEwYWQtZTVmZDliNWJhMzg3IiwicyI6Ik5URmpaRGt3T1RNdE1HUTVPQzAwT0dZMUxUbGpPV1V0WlRjMU5UY3haR00wT1dRMy
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiNzMwNmE5OGE0NzhlNDY2MjE1NTZjMzE4YWNhMzBmNjEiLCJ0IjoiNzk2YzhkZmYtOWY0OC00MjBkLThiNjctNzIzZGY4M2M2OWY5IiwicyI6IllXTTVZbU0zTlRrdE4yUXhNeTAwWVRKakxUaGtNREV0TURJMU1qQmtORFV3WmpnNSJ9
